c633feee2a6ac4496200159a8aa5aac5810c9192
Ankush263 <ankushbanik263@gmail.com>
2022-05-04 09:04:45 +0530

    Add firebase.js

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB-VxbQ9lgu31G2wPwF642C16T27dV9dhU",
+  authDomain: "clone-14626.firebaseapp.com",
+  projectId: "clone-14626",
+  storageBucket: "clone-14626.appspot.com",
+  messagingSenderId: "4371515081",
+  appId: "1:4371515081:web:c3ce3cc0cc89487efd2f18",
+  measurementId: "G-D73HWNXCLX"
+};
