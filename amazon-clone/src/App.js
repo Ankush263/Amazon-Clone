5d3f693c4bf332a918276ba73b691dfe79a5de5f
Ankush263 <ankushbanik263@gmail.com>
2022-05-05 11:30:09 +0530

    Update App.js

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,13 +1,25 @@
+import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
