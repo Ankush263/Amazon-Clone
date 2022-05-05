a00a06e828cbc647177b638be316c90a3074baf6
Ankush263 <ankushbanik263@gmail.com>
2022-05-05 09:44:01 +0530

    update product.js

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,25 +1,23 @@
 import React from 'react'
 import './Product.css'
-import TheLeanStartup from '../src/images/the-lean-startup.jpg'
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className='product'>
       <div className='product__info'>
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className='product__price'>
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className='product__rating'>
-          <p>🌟</p>
+          {Array(rating).fill().map((_, i) => (
+            <p>🌟</p>
+          ))}
         </div>
       </div>
 
-      <img 
-        src={TheLeanStartup}
-        alt='The lean startup'
-      />
+      <img src={image} />
 
       <button>Add to Busket</button>
     </div>
