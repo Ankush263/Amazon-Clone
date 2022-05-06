bc8b7200979c8233acd58a3b23cc17fbd6da3df2
Ankush263 <ankushbanik263@gmail.com>
2022-05-06 10:09:53 +0530

    setup subtotal

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import './Subtotal.css'
+import CurrencyFormat from 'react-currency-format'
 
 const Subtotal = () => {
   return (
     <div className='subtotal'>
-      {/* 1.51.23 */}
+      <CurrencyFormat 
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items):<strong>0</strong>
+            </p>
+            <small className='subtotal__gift'>
+              <input type="checkbox" />
+              This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={'text'}
+        thousandSeparator={true}
+        prefix={'Rs'}
+      />
     </div>
   )
 }
