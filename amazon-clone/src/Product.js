8b0bc4a4c1aaf6f66e15d5edab92c3b3f67b2024
Ankush263 <ankushbanik263@gmail.com>
2022-05-06 13:59:39 +0530

    create addToBasket method

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from './StateProvider'
 
 function Product({ id, title, image, price, rating }) {
+  const [state, dispatch] = useStateValue()
+
+  const addToBasket =() => {
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating
+      }
+    })
+  }
+
+
   return (
     <div className='product'>
       <div className='product__info'>
@@ -19,7 +36,7 @@ function Product({ id, title, image, price, rating }) {
 
       <img src={image} alt='' />
 
-      <button>Add to Busket</button>
+      <button onClick={addToBasket}>Add to Busket</button>
     </div>
   )
 }
