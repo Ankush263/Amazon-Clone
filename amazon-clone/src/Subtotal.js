bbcc4a9bfb267f56a270007095ca7faf0aa82051
Ankush263 <ankushbanik263@gmail.com>
2022-05-07 09:52:01 +0530

    Add new functionalityu in subtotal

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format'
 import { useStateValue } from './StateProvider'
+import { getBasketTotal } from './reducer'
 
 const Subtotal = () => {
   const [{ basket }, dispatch] = useStateValue()
-  let totalPrice = 0
   
   return (
     <div className='subtotal'>
@@ -15,9 +15,7 @@ const Subtotal = () => {
             <p>
               Subtotal ({basket?.length}{basket?.length <= 1 ? ' item' : ' items'}):
               <strong>
-                {basket.forEach(element => (
-                  totalPrice += element.price
-                ))}${totalPrice}
+                {value}
               </strong>
             </p>
             <small className='subtotal__gift'>
@@ -27,7 +25,7 @@ const Subtotal = () => {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={'text'}
         thousandSeparator={true}
         prefix={'$'}
