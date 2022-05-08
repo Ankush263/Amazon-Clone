6c2bebc331bcac72ab4de21a17e0b43e0e030ae6
Ankush263 <ankushbanik263@gmail.com>
2022-05-08 09:17:00 +0530

    Add link

---
+++ amazon-clone/src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <h1>Log</h1>
+      <img  />
+    </div>
+  )
+}
+
+export default Login
