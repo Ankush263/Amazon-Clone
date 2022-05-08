15a976dd0ccbc23fbfaa57d3a3ac07a80a890b95
Ankush263 <ankushbanik263@gmail.com>
2022-05-08 09:16:35 +0530

    update

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Login from './Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Checkout from './Checkout';
 
