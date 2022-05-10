b86a68751315fac3fe5ab870479b05552bfdac66
Ankush263 <ankushbanik263@gmail.com>
2022-05-10 09:02:11 +0530

    update

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Login from './Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Checkout from './Checkout';
+import { auth } from 'firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{}, dispatch] = useStateValue()
+
+  useEffect(() => {
+    auth().onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>', authUser)
+
+      if(authUser) {
+
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }else {
+
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
   return (
     <Router>
       <div className="app">
