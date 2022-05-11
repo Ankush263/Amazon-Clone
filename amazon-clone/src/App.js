d036a4fd6b2dec20a6d8f742b470f03d5293c87c
Ankush263 <ankushbanik263@gmail.com>
2022-05-11 09:34:19 +0530

    introduce payment route

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -44,6 +44,10 @@ function App() {
             <Header />
             <Checkout />
           </Route>
+          <Route path="/payment">
+            <Header />
+            <h1>Payment Route</h1>
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
