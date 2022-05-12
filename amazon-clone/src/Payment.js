ea7308edd9ea1cf4e843267bfb8927779fb6ddb9
Ankush263 <ankushbanik263@gmail.com>
2022-05-12 11:00:45 +0530

    update

--- amazon-clone/src/Payment.js
+++ amazon-clone/src/Payment.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CheckoutProduct from './CheckoutProduct'
 import './Payment.css'
 import { useStateValue } from './StateProvider'
 import { Link } from 'react-router-dom'
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
+import CurrencyFormat from 'react-currency-format'
+import { getBasketTotal } from './reducer'
 
 
 const Payment = () => {
@@ -12,6 +14,18 @@ const Payment = () => {
 
   const stripe = useStripe()
   const elements = useElements()
+
+  const [error, setError] = useState(null)
+  const [disabled, setDisabled] = useState(true)
+
+  const handleSubmit = (e) => {
+    
+  }
+
+  const handleChange = (e) => {
+    setDisabled(e.empty)
+    setError(e.error ? e.error.message : '')
+  }
 
   return (
     <div className='payment'>
@@ -57,8 +71,20 @@ const Payment = () => {
           </div>
 
           <div className='payment__details'>
-            <form>
-              <CardElement />
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+              <div className='payment__priceContainer'>
+                <CurrencyFormat 
+                  renderText={(value) => (
+                    <h3>Order Total: {value}</h3>
+                  )}      
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={'text'}
+                  thousandSeparator={true}
+                  prefix={'$'}
+                />
+              </div>
             </form>
           </div>
         </div>
