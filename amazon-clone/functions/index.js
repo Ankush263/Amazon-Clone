51026e5c1724e58cea738e025d489dc9eceaabdf
Ankush263 <ankushbanik263@gmail.com>
2022-05-13 11:31:16 +0530

    setup

--- amazon-clone/functions/index.js
+++ amazon-clone/functions/index.js
@@ -1,9 +1,23 @@
 const functions = require("firebase-functions");
+const express = require('express')
+const cors = require('cors')
+const stripe = require('stripe')
+('sk_test_51KyTYDSBWqHPKSDaPL3NjDXXCtNNryJmjZYJZQ0Jhh7vqbkjKvnsi29oaZ5CpSAJT9h8gBi8P9D4e8IICNaPAmOB00sIWeeCpN')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//API
+
+// - App config
+const app = express()
+
+// - Middlewares
+app.use(cors({ origin: true }))
+app.use(express.json())
+
+// - API routes
+app.get('/', (req, res) => {
+  res.status(200).send('hello Ankush')
+})
+
+// - Listen commands
+exports.api = functions.https.onRequest(app)
+//http://localhost:5001/clone-14626/us-central1/api
