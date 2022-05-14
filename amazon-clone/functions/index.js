37e71678c3658c486ebef170d2bc23b0203e5e49
Ankush263 <ankushbanik263@gmail.com>
2022-05-14 09:40:23 +0530

    update

--- amazon-clone/functions/index.js
+++ amazon-clone/functions/index.js
@@ -1,23 +1,40 @@
 const functions = require("firebase-functions");
-const express = require('express')
-const cors = require('cors')
-const stripe = require('stripe')
-('sk_test_51KyTYDSBWqHPKSDaPL3NjDXXCtNNryJmjZYJZQ0Jhh7vqbkjKvnsi29oaZ5CpSAJT9h8gBi8P9D4e8IICNaPAmOB00sIWeeCpN')
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51KyTYDSBWqHPKSDaPL3NjDXXCtNNryJmjZYJZQ0Jhh7vqbkjKvnsi29oaZ5CpSAJT9h8gBi8P9D4e8IICNaPAmOB00sIWeeCpN"
+);
 
-//API
+// API
 
 // - App config
-const app = express()
+const app = express();
 
 // - Middlewares
-app.use(cors({ origin: true }))
-app.use(express.json())
+app.use(cors({ origin: true }));
+app.use(express.json());
 
 // - API routes
-app.get('/', (req, res) => {
-  res.status(200).send('hello Ankush')
-})
+app.get("/", (request, response) => response.status(200).send("hello world"));
 
-// - Listen commands
-exports.api = functions.https.onRequest(app)
-//http://localhost:5001/clone-14626/us-central1/api
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "usd",
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
+
+// Example endpoint
+// http://localhost:5001/challenge-4b2b2/us-central1/api
