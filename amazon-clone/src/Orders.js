d77c7778f91212aea0f4d0842d061ebb6fcb3d3a
Ankush263 <ankushbanik263@gmail.com>
2022-05-15 09:53:49 +0530

    introduce Order.js

---
+++ amazon-clone/src/Orders.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Orders.css'
+
+const Orders = () => {
+  return (
+    <div className='orders'>
+      <h1>Orders</h1>
+    </div>
+  )
+}
+
+export default Orders
